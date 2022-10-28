99dc9d7967f3e06821c9c5f4eec615fee61a3801
Dominic R <110837479+jD2R@users.noreply.github.com>
2022-10-28 14:14:45 -0400

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,6 @@
 const palindrome = str => {
   // strip the string of all non-alphanumeric characters
-  const s = str.replace(/[^0-9a-zA-Z]/, '').toUpperCase()
+  const s = str.replace(/[^0-9a-zA-Z]/, '').toUpperCase();
   // return if the string equals the reversed string (palindrome)
-  return s === [...s].reverse().join('')
+  return s === [...s].reverse().join('');
 }
