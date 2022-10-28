b21855e5501bb85627879e857b7b2460a3ff896f
Dominic R <110837479+jD2R@users.noreply.github.com>
2022-10-28 10:44:17 -0400

    Create palindrome.js

---
+++ palindrome.js
@@ -0,0 +1,10 @@
+function palindrome (str) {
+  // strip the string of all non-alphanumeric characters
+  let s = [...str].map(s => {
+    if (/^[a-z0-9]+$/i.test(s)) {
+    	return s.toUpperCase();
+    }
+  }).join('');
+  // return if the string equals the reversed string (palindrome)
+  return (s === [...s].reverse().join(''));
+}
